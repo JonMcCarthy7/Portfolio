7140a45834b5c245ffe6505f9d0f10d91a2dc3ef
Jon McCarthy <jtmcc93@gmail.com>
2016-12-16 22:42:55 -0700

    Update Portfolio

--- stylesheets/java.js
+++ stylesheets/java.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
     $('.parallax').parallax();
   });
-      
+
+
+
+  //Smooth scroll to each sections
+  $("#button1").click(function(){
+      $('html, body').animate({
+          scrollTop: $("#contact").offset().top - 63
+      }, 1200);
+  });
+  $("#button2").click(function(){
+      $('html, body').animate({
+          scrollTop: $("#about").offset().top - 63
+      }, 1200);
+  });
+  $("#button3").click(function(){
+      $('html, body').animate({
+          scrollTop: $("#work").offset().top - 63
+      }, 1000);
+  });
